18bc10100948c7029e6bed963a84413c48b15c73
jekakoba <endlesss2k2k@gmail.com>
2024-02-09 12:25:13 +0200

    gallery

--- src/js/files/gallery.js
+++ src/js/files/gallery.js
@@ -13,10 +13,11 @@ import { flsModules } from "./modules.js";
 // Підключення базового набору функціоналу
 import lightGallery from 'lightgallery';
 
+
 // Плагіни
 // lgZoom, lgAutoplay, lgComment, lgFullscreen, lgHash, lgPager, lgRotate, lgShare, lgThumbnail, lgVideo, lgMediumZoom
 // import lgThumbnail from 'lightgallery/plugins/thumbnail/lg-thumbnail.min.js'
-//import lgZoom from 'lightgallery/plugins/zoom/lg-zoom.min.js'
+// import lgZoom from 'lightgallery/plugins/zoom/lg-zoom.min.js'
 
 // Базові стилі
 import '@scss/libs/gallery/lightgallery.scss';
@@ -37,6 +38,22 @@ import '@scss/libs/gallery/lightgallery.scss';
 // import '@scss/libs/gallery/lightgallery-bundle.scss';
 
 // Запуск
+// const galleries = document.querySelectorAll('[data-gallery]');
+// if (galleries.length) {
+// 	let galleyItems = [];
+// 	galleries.forEach(gallery => {
+// 		galleyItems.push({
+// 			gallery,
+// 			galleryClass: lightGallery(gallery, {
+// 				// plugins: [lgZoom, lgThumbnail],
+// 				licenseKey: '7EC452A9-0CFD441C-BD984C7C-17C8456E',
+// 				speed: 500,
+// 			})
+// 		})
+// 	});
+// 	// Додаємо в об'єкт модулів
+// 	flsModules.gallery = galleyItems;
+// }
 const galleries = document.querySelectorAll('[data-gallery]');
 if (galleries.length) {
 	let galleyItems = [];
@@ -44,9 +61,11 @@ if (galleries.length) {
 		galleyItems.push({
 			gallery,
 			galleryClass: lightGallery(gallery, {
+				// plugins: [lgZoom],
 				// plugins: [lgZoom, lgThumbnail],
 				licenseKey: '7EC452A9-0CFD441C-BD984C7C-17C8456E',
 				speed: 500,
+				selector: '.product-images__slide',
 			})
 		})
 	});
